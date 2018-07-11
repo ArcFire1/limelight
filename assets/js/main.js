726bfee673534df4edf778fdb9ca8ba311308235
Alexei <storm371@gmail.com>
2018-07-11 13:11:58 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items: 1,
+      center: true,
+      loop: true,
+      autoplay: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 5000,
+      smartSpeed: 1500
+  });
+});
